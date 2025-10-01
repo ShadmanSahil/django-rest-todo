beaac5c7b6813843e238cd413aed676e46398088
Shadman Sahil Chowdhury <shadman@Shadmans-MacBook-Air.local>
2025-10-01 16:25:13 +0200

    feat(settings): configure DRF with Firebase auth and default permissions

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -46,6 +46,13 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "firebase.FirebaseAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+
 }
 
 MIDDLEWARE = [
